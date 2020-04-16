138ebfe7b55b70743146128971f63c3e71a44bd9
qnix <qnix@users.noreply.github.com>
2020-04-16 10:08:54 -0700

    init clojure and clojure script
    Cider rocks!

---
+++ lisp/init-clojure.el
@@ -0,0 +1,8 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Clojure/Cider
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package clojure-mode)
+(use-package inf-clojure)
+(use-package cider)
+
+(provide 'init-clojure)
